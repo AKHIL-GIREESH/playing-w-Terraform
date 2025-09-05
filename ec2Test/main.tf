3db2aa49152e95d45b37beb88a7c88f4f206d7c3
AKHIL-GIREESH <94656684+AKHIL-GIREESH@users.noreply.github.com>
2025-09-05 23:09:24 +0530

    Update main.tf

--- ec2Test/main.tf
+++ ec2Test/main.tf
@@ -1,4 +1,34 @@
 resource "aws_instance" "firstEC2" {
-  ami           = "ami-04b4f1a9cf54c11d0"
-  instance_type = "t2.micro"
+  ami                    = "ami-0e35ddab05955cf57"
+  instance_type          = "t2.micro"
+  user_data              = <<-EOF
+                            #!/bin/bash
+                            sudo touch /var/www/html/index.html
+                            sudo echo "Hello, world!" > /var/www/html/index.html
+                            EOF
+  key_name               = aws_key_pair.rsaa.id
+  vpc_security_group_ids = [aws_security_group.ssh-access.id]
+  tags = {
+    Name        = "firstEC2TF"
+    Description = "First ever EC2 using TF"
+  }
+}
+
+resource "aws_key_pair" "rsaa" {
+  public_key = file("/Users/akhilgireesh/.ssh/id_rsa.pub")
+}
+
+resource "aws_security_group" "ssh-access" {
+  name        = "ssh-access"
+  description = "To allow ssh access from the internet"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "publicIp" {
+  value = aws_instance.firstEC2.public_ip
 }
